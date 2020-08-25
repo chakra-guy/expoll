4c98340ac168564f5b47f060b95afebc3ab7daa3
Tamas Soos <soostamas.hu@gmail.com>
2020-08-25 15:19:53 +0100

    Removing doctests

--- lib/ex_poll/polls.ex
+++ lib/ex_poll/polls.ex
@@ -8,40 +8,12 @@ defmodule ExPoll.Polls do
 
   alias ExPoll.Polls.{Poll, Option}
 
-  def options_query do
-    from o in Option,
-      left_join: v in assoc(o, :votes),
-      group_by: o.id,
-      select_merge: %{vote_count: count(v.id)}
-  end
+  # POLL
 
-  @doc """
-  Returns the list of polls.
-
-  ## Examples
-
-      iex> list_polls()
-      [%Poll{}, ...]
-
-  """
   def list_polls do
     Repo.all(Poll)
   end
 
-  @doc """
-  Gets a single poll.
-
-  Raises `Ecto.NoResultsError` if the Poll does not exist.
-
-  ## Examples
-
-      iex> get_poll!(123)
-      %Poll{}
-
-      iex> get_poll!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_poll!(id) do
     query =
       from p in Poll,
@@ -51,85 +23,35 @@ defmodule ExPoll.Polls do
     Repo.one!(query)
   end
 
-  @doc """
-  Creates a poll.
-
-  ## Examples
-
-      iex> create_poll(%{field: value})
-      {:ok, %Poll{}}
-
-      iex> create_poll(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_poll(attrs \\ %{}) do
     %Poll{}
     |> Poll.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a poll.
-
-  ## Examples
-
-      iex> update_poll(poll, %{field: new_value})
-      {:ok, %Poll{}}
-
-      iex> update_poll(poll, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_poll(%Poll{} = poll, attrs) do
     poll
     |> Poll.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a poll.
-
-  ## Examples
-
-      iex> delete_poll(poll)
-      {:ok, %Poll{}}
-
-      iex> delete_poll(poll)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_poll(%Poll{} = poll) do
     Repo.delete(poll)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking poll changes.
-
-  ## Examples
-
-      iex> change_poll(poll)
-      %Ecto.Changeset{data: %Poll{}}
-
-  """
   def change_poll(%Poll{} = poll, attrs \\ %{}) do
     Poll.changeset(poll, attrs)
   end
 
-  @doc """
-  Gets a single option.
+  # OPTION
 
-  Raises `Ecto.NoResultsError` if the Option does not exist.
+  def options_query do
+    from o in Option,
+      left_join: v in assoc(o, :votes),
+      group_by: o.id,
+      select_merge: %{vote_count: count(v.id)}
+  end
 
-  ## Examples
-
-      iex> get_option!(123)
-      %Option{}
-
-      iex> get_option!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_option!(id) do
     query =
       from o in options_query(),
@@ -138,18 +60,6 @@ defmodule ExPoll.Polls do
     Repo.one!(query)
   end
 
-  @doc """
-  Creates a option.
-
-  ## Examples
-
-      iex> create_option(%{field: value})
-      {:ok, %Option{}}
-
-      iex> create_option(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_option(%Poll{} = poll, attrs \\ %{}) do
     poll
     |> Ecto.build_assoc(:options)
@@ -157,65 +67,20 @@ defmodule ExPoll.Polls do
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a option.
-
-  ## Examples
-
-      iex> update_option(option, %{field: new_value})
-      {:ok, %Option{}}
-
-      iex> update_option(option, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_option(%Option{} = option, attrs) do
     option
     |> Option.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a option.
-
-  ## Examples
-
-      iex> delete_option(option)
-      {:ok, %Option{}}
-
-      iex> delete_option(option)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_option(%Option{} = option) do
     Repo.delete(option)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking option changes.
-
-  ## Examples
-
-      iex> change_option(option)
-      %Ecto.Changeset{data: %Option{}}
-
-  """
   def change_option(%Option{} = option, attrs \\ %{}) do
     Option.changeset(option, attrs)
   end
 
-  @doc """
-  Creates a vote.
-
-  ## Examples
-
-      iex> create_vote(%{field: value})
-      {:ok, %Vote{}}
-
-      iex> create_vote(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_vote(%Option{} = option) do
     option
     |> Ecto.build_assoc(:votes)
